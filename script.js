861826f8b67a25171e025cc51f954961a9bf1241
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2024-10-04 23:32:32 +0200

    Add contents (#1)
    * wip
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>
    
    * More things
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>
    
    * Add toc
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>
    
    * More small changes
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>
    
    * Add important links on the top
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 import http from "k6/http";
-import {sleep} from "k6";
 
-const BASE_URL = __ENV.BASE_URL || 'http://localhost:3333';
-
-export let options = {
-  stages: [
-    { duration: "5s", target: 10 },
-    { duration: "10s", target: 10 },
-    { duration: "5s", target: 0 },
-  ],
-  thresholds: {
-    "http_req_duration": ["p(95)<5000"],
-  },
-};
+const BASE_URL = __ENV.BASE_URL || "http://localhost:3333";
 
 export default function () {
   let restrictions = {
@@ -21,14 +9,13 @@ export default function () {
     excludedIngredients: ["pepperoni"],
     excludedTools: ["knife"],
     maxNumberOfToppings: 6,
-    minNumberOfToppings: 2
-  }
+    minNumberOfToppings: 2,
+  };
   let res = http.post(`${BASE_URL}/api/pizza`, JSON.stringify(restrictions), {
     headers: {
-      'Content-Type': 'application/json',
-      'X-User-ID': 23423,
+      "Content-Type": "application/json",
+      "X-User-ID": 23423,
     },
   });
   console.log(`${res.json().pizza.name} (${res.json().pizza.ingredients.length} ingredients)`);
-  sleep(1);
 }
